efbda14a569c4c81e1541d61ec016cfcc7ed8f11
PrzemyslawKlys <pklys@evotec.pl>
2018-05-12 17:37:39 +0200

    Removed Write-Color (Use Import-Module PSWriteColor)

--- PSEventViewer.psm1
+++ PSEventViewer.psm1
@@ -1188,39 +1188,6 @@ function Get-UserChanges($Servers, $Dates, $ReportOptions) {
     Write-Color @script:WriteParameters "[i] Ending ", "User Lockouts Report", " for dates from: ", "$($Dates.DateFrom)", " to: ", "$($Dates.DateTo)", "." -Color White, Green, White, Green, White, Green, White
     return $UserChangesOutput
 }
-function Write-Color([String[]]$Text, [ConsoleColor[]]$Color = "White", [int]$StartTab = 0, [int] $LinesBefore = 0, [int] $LinesAfter = 0, [string] $LogFile = "", $TimeFormat = "yyyy-MM-dd HH:mm:ss", [switch] $ShowTime, [switch] $NoNewLine) {
-    # Author: przemyslaw.klys at evotec.pl
-    # version 0.3
-    # - added -ShowTime
-    # - added -NoNewLine
-    # version 0.2
-    # - added logging to file
-    # version 0.1
-    # - first draft
-    #
-    # Notes:
-    # - TimeFormat https://msdn.microsoft.com/en-us/library/8kb3ddd4.aspx
-
-    $DefaultColor = $Color[0]
-    if ($LinesBefore -ne 0) {  for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host "`n" -NoNewline } } # Add empty line before
-    if ($ShowTime) { Write-Host "[$([datetime]::Now.ToString($TimeFormat))]" -NoNewline} # Add Time before output
-    if ($StartTab -ne 0) {  for ($i = 0; $i -lt $StartTab; $i++) { Write-Host "`t" -NoNewLine } }  # Add TABS before text
-    if ($Color.Count -ge $Text.Count) {
-        for ($i = 0; $i -lt $Text.Length; $i++) { Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine }
-    } else {
-        for ($i = 0; $i -lt $Color.Length ; $i++) { Write-Host $Text[$i] -ForegroundColor $Color[$i] -NoNewLine }
-        for ($i = $Color.Length; $i -lt $Text.Length; $i++) { Write-Host $Text[$i] -ForegroundColor $DefaultColor -NoNewLine }
-    }
-    if ($NoNewLine -eq $true) { Write-Host -NoNewline } else { Write-Host } # Support for no new line
-    if ($LinesAfter -ne 0) {  for ($i = 0; $i -lt $LinesAfter; $i++) { Write-Host "`n" } }  # Add empty line after
-    if ($LogFile -ne "") {
-        $TextToFile = ""
-        for ($i = 0; $i -lt $Text.Length; $i++) {
-            $TextToFile += $Text[$i]
-        }
-        Write-Output "[$([datetime]::Now.ToString($TimeFormat))]$TextToFile" | Out-File $LogFile -Encoding unicode -Append
-    }
-}
 function Send-Email ([hashtable] $EmailParameters, [string] $Body = "", $Attachment = $null, [string] $Subject = "", $To = "") {
     #     $SendMail = Send-Email -EmailParameters $EmailParameters -Body $EmailBody -Attachment $Reports -Subject $TemporarySubject
     #  Preparing the Email properties
@@ -1838,7 +1805,7 @@ function Start-Report([hashtable] $Dates, [hashtable] $EmailParameters, [hashtab
     $EmailBody = Set-EmailWordReplacements -Body $EmailBody -Replace '**TimeToGenerateMilliseconds**' -ReplaceWith $time.Elapsed.Milliseconds
     $Time.Stop()
 
-    $script:TimeToGenerateReports | ConvertTo-Json
+    #$script:TimeToGenerateReports | ConvertTo-Json
 
     # Sending email - finalizing package
     if ($ReportOptions.SendMail -eq $true) {
