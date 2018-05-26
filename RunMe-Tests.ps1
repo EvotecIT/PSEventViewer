e75dc444417351b531a26e7985489918d7990f63
PrzemyslawKlys <pklys@evotec.pl>
2018-05-26 23:58:19 +0200

    Few more tests to verify functionality

--- RunMe-Tests.ps1
+++ RunMe-Tests.ps1
@@ -4,21 +4,25 @@ Write-Color 'Start processing events - Tests for expected output' -Color Red
 # #, 4722, 4723, 4724, 4725, 4726, 4738, 4740, 4767
 $TestServers = 'AD1.ad.evotec.xyz'
 $ID = 104
-$TestEvents1 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents1 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 16384
-$TestEvents2 = Get-Events -Machine $TestServers -Id $ID -LogName 'Application' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents2 = Get-Events -Machine $TestServers -Id $ID -LogName 'Application' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 4634
-$TestEvents3 = Get-Events -Machine $TestServers -Id $ID -LogName 'Security' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents3 = Get-Events -Machine $TestServers -Id $ID -LogName 'Security' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 4688
-$TestEvents4 = Get-Events -Machine $TestServers -Id $ID -LogName 'Security' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents4 = Get-Events -Machine $TestServers -Id $ID -LogName 'Security' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 105
-$TestEvents5 = Get-Events -Machine $TestServers -Id $ID -LogName 'Application' -MaxEvents 5  #-DisableParallel  #-Verbose
+$TestEvents5 = Get-Events -Machine $TestServers -Id $ID -LogName 'Application' -MaxEvents 1  #-DisableParallel  #-Verbose
 $ID = 7036
-$TestEvents6 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents6 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 32
-$TestEvents7 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents7 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 1 #-DisableParallel #-Verbose
 $ID = 1014
-$TestEvents8 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 5 #-DisableParallel #-Verbose
+$TestEvents8 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 1 #-DisableParallel #-Verbose
+$ID = 8198
+$TestEvents9 = Get-Events -Machine $TestServers -Id $ID -LogName 'Application' -MaxEvents 1  #-DisableParallel  #-Verbose
+$ID = 10154
+$TestEvents10 = Get-Events -Machine $TestServers -Id $ID -LogName 'System' -MaxEvents 1 #-DisableParallel #-Verbose
 
 Write-Color 'Jump 1' -Color Green
 $TestEvents1 | fl Channel, BackupPath, SubjectDomainName, SubjectUserName
@@ -36,4 +40,8 @@ Write-Color 'Jump 7' -Color Green
 $TestEvents7 | fl NoNameB1, NoNameB2
 Write-Color 'Jump 8' -Color Green
 $TestEvents8 | fl QueryName, AddressLength
+Write-Color 'Jump 9' -Color Green
+$TestEvents9 | fl NoNameA0, NoNameA1
+Write-Color 'Jump 10' -Color Green
+$TestEvents10 | fl spn1, spn2
 Write-Color 'End processing events - Tests for expected output' -Color Red
