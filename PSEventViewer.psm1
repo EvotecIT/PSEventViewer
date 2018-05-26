330b8e028b7fb8f99cb54194c6021beb8c157c94
PrzemyslawKlys <pklys@evotec.pl>
2018-05-26 23:43:57 +0200

    Removed unused, duplicate method

--- PSEventViewer.psm1
+++ PSEventViewer.psm1
@@ -22,24 +22,6 @@ function Add-ToHashTable($Hashtable, $Key, $Value) {
         $Hashtable.Add($Key, $Value)
     }
 }
-Function Split-Every($list, $count = 4) {
-    $aggregateList = @()
-
-    $blocks = [Math]::Floor($list.Count / $count)
-    $leftOver = $list.Count % $count
-    $start = $end = 0
-    for ($i = 0; $i -lt $blocks; $i++) {
-        $end = $count * ($i + 1) - 1
-
-        $aggregateList += @(, $list[$start..$end])
-        $start = $end + 1
-    }
-    if ($leftOver -gt 0) {
-        $aggregateList += @(, $list[$start..($end + $leftOver)])
-    }
-
-    $aggregateList
-}
 function Split-Array {
     <#
         .SYNOPSIS
