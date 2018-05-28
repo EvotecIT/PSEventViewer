658565020d33d9b94b7e6fe7d78b4b1ff8c206fe
PrzemyslawKlys <pklys@evotec.pl>
2018-05-28 11:33:00 +0200

    Few examples

--- RunMe-Tests2.ps1
+++ RunMe-Tests2.ps1
@@ -4,3 +4,5 @@ Get-WinEvent -FilterHashtable @{ LogName = 'Security'; Id = 1102 } -ComputerName
 # Example new way
 Import-Module PSEventViewer -Force
 Get-Events -LogName 'Security' -Id 1102 -ComputerName 'AD1' | Format-List *
+Get-Events -LogName 'Setup' -Id 9 -ComputerName 'AD1' -MaxEvents 1 | Format-List *
+Get-Events -LogName 'Setup' -Id 2 -ComputerName 'AD1' -MaxEvents 10 | Format-List *
