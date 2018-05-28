43de524d70d713718e8b3b0e7260579c705e37dc
PrzemyslawKlys <pklys@evotec.pl>
2018-05-28 11:33:31 +0200

    Updates to examples

--- RunMe-Tests3.ps1
+++ RunMe-Tests3.ps1
@@ -9,4 +9,4 @@ Clear-Host
 Import-Module PSEventViewer
 Get-Events -LogName 'Setup' -ID 2 -ComputerName 'AD1' -MaxEvents 1 | Format-List *
 Get-Events -LogName 'Setup' -ID 2 -ComputerName 'AD1' -MaxEvents 10 | Format-List MachineName, IntendedPackageState, PackageIdentifier, Date, ErrorCode
-Get-Events -LogName 'Setup' -ID 2 -ComputerName 'AD1' -MaxEvents 10 | Where { $_.PackageIdentifier -eq 'KB4103723' } | Format-List MachineName, IntendedPackageState, PackageIdentifier, Date, ErrorCode
+Get-Events -LogName 'Setup' -ID 2 -ComputerName 'AD1' -MaxEvents 10 | Where-Object { $_.PackageIdentifier -eq 'KB4103723' } | Format-List MachineName, IntendedPackageState, PackageIdentifier, Date, ErrorCode
