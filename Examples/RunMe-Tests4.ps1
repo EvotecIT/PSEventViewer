72059e83db2e491d7d99dde40d11d551320ee5ae
PrzemyslawKlys <pklys@evotec.pl>
2018-07-17 21:55:19 +0200

    Small example updates

--- Examples/RunMe-Tests4.ps1
+++ Examples/RunMe-Tests4.ps1
@@ -1,4 +1,5 @@
-$IDRequiringSplitOver23 = 1102, 5136, 5137, 5141, 4364, 4647, 4672, 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758, 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762, 4725, 4722, 4725, 4767, 4723, 4724, 4726, 4740, 4720, 4738
+# Following won't work due to more events then 234
 
-Get-WinEvent -FilterHashtable @{ LogName = 'Security'; ID = $IDRequiringSplitOver23 }
+$IDRequiringSplitOver23 = 1102, 5136, 5137, 5141, 4364, 4647, 4672, 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758, 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762, 4725, 4722, 4725, 4767, 4723, 4724, 4726, 4740, 4720, 4738
+Get-WinEvent -FilterHashtable @{ LogName = 'ForwardedEvents'; ID = $IDRequiringSplitOver23 } -Verbose
 Get-Events -Id $IDRequiringSplitOver23 -LogName 'Security'
