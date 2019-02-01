7ce9199fad7b617d86aa0321c28fcdc6a5c261a6
PrzemyslawKlys <pklys@evotec.pl>
2019-02-01 19:18:27 +0100

    Update

--- Examples/RunMe-Tests14.ps1
+++ Examples/RunMe-Tests14.ps1
@@ -6,6 +6,7 @@ $List = @()
 #$List += @{ Server = 'C:\MyEvents\Archive-Security-2018-09-15-09-27-52-679.evtx'; LogName = 'Security'; EventID = '5136', '5137'; Type = 'File' }
 #$List += @{ Server = 'Evo1'; LogName = 'Setup'; EventID = 2; Type = 'Computer'; }
 #$List += @{ Server = 'AD1.ad.evotec.xyz'; LogName = 'Security'; EventID = 4720, 4738, 5136, 5137, 5141, 4722, 4725, 4767, 4723, 4724, 4726, 4728, 4729, 4732, 4733, 4746, 4747, 4751, 4752, 4756, 4757, 4761, 4762, 4785, 4786, 4787, 4788, 5136, 5137, 5141, 5136, 5137, 5141, 5136, 5137, 5141; Type = 'Computer' }
-$List += @{ Server = 'Evo1'; LogName = 'Application'; Type = 'Computer'; MaxEvents = 15; Level = 'Error', 'Informational'; }
+$List += @{ Server = 'Evo1'; LogName = 'Security'; Type = 'Computer'; MaxEvents = 15; Level = 'Informational'; Keywords = 'AuditSuccess' }
+$List += @{ Server = 'Evo1'; LogName = 'Security'; Type = 'Computer'; MaxEvents = 15; Level = 'Informational'; Keywords = 'AuditFailure' }
 $Output4 = Get-Events -ExtendedInput $List -Verbose
 $Output4 | ft Computer, Date, LevelDisplayName
