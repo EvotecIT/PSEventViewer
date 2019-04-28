443b8ed423eeb977d516a09bf63fe4c95910f31b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-28 18:49:03 +0200

    Update

--- Examples/RunMe-Test16.ps1
+++ Examples/RunMe-Test16.ps1
@@ -3,5 +3,7 @@
 $Date = (Get-Date).AddDays(-3)
 $Date1 = Get-Date
 
-$Events = Get-Events -Machine AD1 -DateFrom $Date -DateTo $Date1 -ID 4738 -LogName 'Security'
-$Events[0] | fl *
+$User = 'Administrator'
+
+$Events = Get-Events -Machine AD1 -DateFrom $Date -DateTo $Date1 -ID 4738 -LogName 'Security' -Verbose -NamedDataFilter @{'SubjectUserName' = $User; 'TargetUserName' = $User }
+$Events
