74e2e75dcf1f4cc8c417fa6cc4c7d8e4592112b7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-07 09:46:46 +0100

    Update example

---
+++ Examples/Example.GetPSEvent.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+Import-Module .\PSEventViewer.psd1 -Force
+
+<#
+$Test = Get-PSEvent -LogName 'Application' -EventId 1026 -Verbose -MaxEvents 1
+$Test | Format-Table
+$Test.Data
+$Test.Data['NoNameA1']
+#>
+
+#Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode ParallelForEach | Format-Table
+
+Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode Parallel #| Select-Object -first 10 | Format-Table
+#Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode ParallelForEachBuiltin | Select-Object -first 10 | Format-Table
---
+++ Examples/RunMe-Test19.ps1
@@ -0,0 +1,5 @@
+﻿
+Import-Module .\PSEventViewer.psd1 -Force
+
+$OK = Get-Events -LogName 'Security' -EventID 4722, 4725, 4740, 4724 -Computer ad1 -DisableParallel
+$OK[1] | Format-List *
