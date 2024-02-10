a7dd3dc5dbcd071be10de608d32d64c73efffafc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-10 09:20:16 +0100

    Test example

--- Examples/Example.GetPSEvent.ps1
+++ Examples/Example.GetPSEvent.ps1
@@ -10,5 +10,25 @@ $Test.Data['NoNameA1']
 
 #Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode ParallelForEach | Format-Table
 
-Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode Parallel #| Select-Object -first 10 | Format-Table
+#Find-GenericEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode Parallel | Select-Object -first 10 | Format-Table
 #Get-PSEvent -LogName 'Security' -EventId 4932,4933 -Verbose -MachineName 'AD1', 'AD2' -Mode ParallelForEachBuiltin | Select-Object -first 10 | Format-Table
+
+#Find-NamedEvent -Type ADComputerChangeDetailed -MachineName AD1,AD2 -Verbose | Format-Table
+
+Find-GenericEvent -LogName 'Security' -EventId 5136, 5137, 5168 -Verbose -MachineName 'AD1', 'AD2', 'AD0' -Mode Parallel
+
+#return
+
+
+$List = [System.Collections.Generic.List[string]]::new()
+$List.Add('AD1')
+$List.Add('AD2')
+$List.Add('AD0')
+
+$IDs = [System.Collections.Generic.List[int]]::new()
+$IDs.Add(5136)
+$IDs.Add(5137)
+$IDs.Add(5168)
+
+$V = [PSEventViewer.EventSearching]::QueryLogsParallel('Security', $Ids, $List)
+$V| Format-Table
