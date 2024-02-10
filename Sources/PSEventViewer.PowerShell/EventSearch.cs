39c4e9efe4c893ab3f78e1c24eb1726fe9ee4590
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-10 09:21:13 +0100

    Update ps testing

--- Sources/PSEventViewer.PowerShell/EventSearch.cs
+++ Sources/PSEventViewer.PowerShell/EventSearch.cs
@@ -3,6 +3,9 @@ using System;
 using System.Management.Automation;
 using System.Xml.Linq;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace PSEventViewer.PowerShell {
 
@@ -22,41 +25,48 @@ namespace PSEventViewer.PowerShell {
         [Parameter(Mandatory = false)] public int MaxEvents = 0;
         [Parameter(Mandatory = false)] public Modes Mode { get; set; } = Modes.Parallel;
 
+        private EventSearching eventSearching;
+
         protected override void BeginProcessing() {
             // Initialize the logger
             var internalLogger = new InternalLogger(false);
-            // Initialize the PowerShell logger, and subscribe to the verbose message event
+            //Initialize the PowerShell logger, and subscribe to the verbose message event
+            //var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, null, this.WriteProgress, this.WriteInformation);
             var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
-            var eventSearching = new EventSearching(internalLogger);
+            eventSearching = new EventSearching(internalLogger);
             eventSearching.Verbose = false;
+            eventSearching.Error = true;
+            eventSearching.Warning = true;
         }
         protected override void ProcessRecord() {
-            if (Mode == Modes.Disabled) {
-                foreach (var machine in MachineName) {
-                    foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents)) {
-                        WriteObject(eventObject);
-                    }
-                }
-            } else if (Mode == Modes.Parallel) {
-                foreach (var eventObject in EventSearching.QueryLogsParallel(LogName, EventId, MachineName, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, NumberOfThreads)) {
-                    WriteObject(eventObject);
-                }
-            } else if (Mode == Modes.ParallelForEach) {
-                var options = new ParallelOptions { MaxDegreeOfParallelism = NumberOfThreads };
-                Parallel.ForEach(MachineName, options, machine => {
-                    foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents)) {
-                        WriteObject(eventObject);
-                    }
-                });
-            } else if (Mode == Modes.ParallelForEachBuiltin) {
-                foreach (var eventObject in EventSearching.QueryLogsParallelForEach(LogName, EventId, MachineName, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, NumberOfThreads)) {
-                    WriteObject(eventObject);
-                }
-            } else if (Mode == Modes.Async) {
 
-            }
+
+            //if (Mode == Modes.Disabled) {
+            //    foreach (var machine in MachineName) {
+            //        foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents)) {
+            //            WriteObject(eventObject);
+            //        }
+            //    }
+            //} else if (Mode == Modes.Parallel) {
+            WriteObject(EventSearching.QueryLogsParallel(LogName, EventId, MachineName, maxThreads: 0), true);
+            //} else if (Mode == Modes.ParallelForEach) {
+            //    var options = new ParallelOptions { MaxDegreeOfParallelism = NumberOfThreads };
+            //    Parallel.ForEach(MachineName, options, machine => {
+            //        foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents)) {
+            //            WriteObject(eventObject);
+            //        }
+            //    });
+            //} else if (Mode == Modes.ParallelForEachBuiltin) {
+            //    foreach (var eventObject in EventSearching.QueryLogsParallelForEach(LogName, EventId, MachineName, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, NumberOfThreads)) {
+            //        WriteObject(eventObject);
+            //    }
+            //} else if (Mode == Modes.Async) {
+
+            //}
         }
         protected override void EndProcessing() {
+
+
             base.EndProcessing();
         }
     }
