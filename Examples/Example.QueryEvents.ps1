2153b0a640b72ba130695c40fe060888f10306f4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-10 18:48:50 +0100

    Show new examples

---
+++ Examples/Example.QueryEvents.ps1
@@ -0,0 +1,4 @@
+﻿Clear-Host
+Import-Module .\PSEventViewer.psd1 -Force
+
+Find-WinEvent -LogName 'Security' -EventId 5136, 5137, 5168 -Verbose -MachineName 'AD1', 'AD2', 'AD0' -ParallelOption Parallel | Format-Table
---
+++ Examples/Example.QueryNamedEvents.ps1
@@ -0,0 +1,4 @@
+﻿Clear-Host
+Import-Module .\PSEventViewer.psd1 -Force
+
+Find-WinEvent -Type ADComputerChangeDetailed, ADUserChangeDetailed -MachineName AD1, AD2, AD0 -Verbose | Format-Table
