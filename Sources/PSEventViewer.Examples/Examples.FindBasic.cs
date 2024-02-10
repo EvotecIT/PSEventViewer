40732c81b059c6c771040879a5c4f8b724173199
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-10 18:49:19 +0100

    Remove not working example

--- Sources/PSEventViewer.Examples/Examples.FindBasic.cs
+++ Sources/PSEventViewer.Examples/Examples.FindBasic.cs
@@ -2,26 +2,26 @@
 
 namespace PSEventViewer.Examples {
     internal partial class Examples {
-        public static void FindEventsTargetedBasic() {
-            foreach (var foundObject in EventSearchingTargeted.FindEvents()) {
-                // Check if the foundObject is of type ADComputerChangeDetailed
+        //public static void FindEventsTargetedBasic() {
+        //    foreach (var foundObject in EventSearchingTargeted.FindEvents()) {
+        //        // Check if the foundObject is of type ADComputerChangeDetailed
 
-                // Console.WriteLine("Event ID: {0}", foundObject.EventID + ", " + foundObject.Type + " " + foundObject.GatheredFrom);
-                Console.WriteLine("Type: " + foundObject.Type + ", " + foundObject.EventID + " " + foundObject.EventID + " " + foundObject.GatheredFrom);
+        //        // Console.WriteLine("Event ID: {0}", foundObject.EventID + ", " + foundObject.Type + " " + foundObject.GatheredFrom);
+        //        Console.WriteLine("Type: " + foundObject.Type + ", " + foundObject.EventID + " " + foundObject.EventID + " " + foundObject.GatheredFrom);
 
-                if (foundObject is ADComputerChangeDetailed adComputerChange) {
-                    // Display the properties of the ADComputerChangeDetailed object
-                    Console.WriteLine("[*] Computer: " + adComputerChange.Computer);
-                    Console.WriteLine("[*] Action: " + adComputerChange.Action);
-                    Console.WriteLine("[*] Operation Type: " + adComputerChange.OperationType);
-                    Console.WriteLine("[*] Who: " + adComputerChange.Who);
-                    Console.WriteLine("[*] When: " + adComputerChange.When);
-                    Console.WriteLine("[*] Object DN: " + adComputerChange.ComputerObject);
-                    Console.WriteLine("[*] Field Changed: " + adComputerChange.FieldChanged);
-                    Console.WriteLine("[*] Field Value: " + adComputerChange.FieldValue);
-                }
-            }
-        }
+        //        if (foundObject is ADComputerChangeDetailed adComputerChange) {
+        //            // Display the properties of the ADComputerChangeDetailed object
+        //            Console.WriteLine("[*] Computer: " + adComputerChange.Computer);
+        //            Console.WriteLine("[*] Action: " + adComputerChange.Action);
+        //            Console.WriteLine("[*] Operation Type: " + adComputerChange.OperationType);
+        //            Console.WriteLine("[*] Who: " + adComputerChange.Who);
+        //            Console.WriteLine("[*] When: " + adComputerChange.When);
+        //            Console.WriteLine("[*] Object DN: " + adComputerChange.ComputerObject);
+        //            Console.WriteLine("[*] Field Changed: " + adComputerChange.FieldChanged);
+        //            Console.WriteLine("[*] Field Value: " + adComputerChange.FieldValue);
+        //        }
+        //    }
+        //}
 
 
         public static void FindEventsTargetedPerType() {
