a50ae7e10e24dfd0ea328995be1b202b85ab652d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-11 20:23:33 +0100

    Add ADUserLogonKerberos class to handle Kerberos authentication ticket requests

--- Sources/PSEventViewer/Rules/ActiveDirectory/Users.cs
+++ Sources/PSEventViewer/Rules/ActiveDirectory/Users.cs
@@ -208,6 +208,44 @@ namespace PSEventViewer.Rules.ActiveDirectory {
     }
 
     /// <summary>
+    /// Active Directory User Logon Kerberos
+    /// 4768: A Kerberos authentication ticket (TGT) was requested
+    /// </summary>
+    public class ADUserLogonKerberos : EventObjectSlim {
+        public string Computer;
+        public string Action;
+        public string ObjectAffected;
+        public string IpAddress;
+        public string IpPort;
+        public string TicketOptions;
+        public string Status;
+        public string TicketEncryptionType;
+        public string PreAuthType;
+        public DateTime When;
+
+        public ADUserLogonKerberos(EventObject eventObject) : base(eventObject) {
+            _eventObject = eventObject;
+            Type = "ADUserLogonKerberos";
+
+            Computer = _eventObject.ComputerName;
+            Action = _eventObject.MessageSubject;
+            ObjectAffected = _eventObject.GetValueFromDataDictionary("TargetUserName", "TargetDomainName", "\\", reverseOrder: true);
+            IpAddress = _eventObject.GetValueFromDataDictionary("IpAddress");
+            IpPort = _eventObject.GetValueFromDataDictionary("IpPort");
+            TicketOptions = _eventObject.GetValueFromDataDictionary("TicketOptions");
+            Status = _eventObject.GetValueFromDataDictionary("Status");
+            TicketEncryptionType = _eventObject.GetValueFromDataDictionary("TicketEncryptionType");
+            PreAuthType = _eventObject.GetValueFromDataDictionary("PreAuthType");
+
+            When = _eventObject.TimeCreated;
+
+            if (IpAddress == "::1") {
+                IpAddress = "Localhost";
+            }
+        }
+    }
+
+    /// <summary>
     /// Active Directory User Unlocked
     /// 4767: A user account was unlocked
     /// </summary>
