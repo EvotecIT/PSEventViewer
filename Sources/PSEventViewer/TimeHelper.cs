ea75e3e9277cac61bdb2024406f7088411108a0e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-12 09:50:39 +0100

    Update methodd to handle more time periods and changed how those time periods behave

--- Sources/PSEventViewer/TimeHelper.cs
+++ Sources/PSEventViewer/TimeHelper.cs
@@ -13,9 +13,13 @@ namespace PSEventViewer {
         Last3Days,
         Last7Days,
         Last14Days,
+        Last1Hours,
+        Last2Hours,
         Last3Hours,
         Last6Hours,
         Last12Hours,
+        Last16Hours,
+        Last24Hours,
         Today,
         Yesterday,
         Everything,
@@ -29,117 +33,120 @@ namespace PSEventViewer {
     }
 
     internal static class TimeHelper {
-        internal static (DateTime? StartTime, DateTime? EndTime) GetTimePeriod(TimePeriod timePeriod) {
+        internal static (DateTime? StartTime, DateTime? EndTime, TimeSpan? LastPeriod) GetTimePeriod(TimePeriod timePeriod) {
             DateTime now = DateTime.UtcNow;
             DateTime? startTime = null;
             DateTime? endTime = null;
+            TimeSpan? lastPeriod = null;
 
             switch (timePeriod) {
                 case TimePeriod.PastHour:
-                    startTime = now.AddHours(-1);
-                    endTime = null;
+                    startTime = new DateTime(now.Year, now.Month, now.Day, now.Hour - 1, 0, 0);
+                    endTime = startTime.Value.AddHours(1);
                     break;
                 case TimePeriod.CurrentHour:
                     startTime = new DateTime(now.Year, now.Month, now.Day, now.Hour, 0, 0);
+                    endTime = startTime.Value.AddHours(1);
                     break;
                 case TimePeriod.PastDay:
-                    startTime = now.AddDays(-1);
-                    endTime = null;
+                    startTime = new DateTime(now.Year, now.Month, now.Day - 1, 0, 0, 0);
+                    endTime = startTime.Value.AddDays(1);
                     break;
                 case TimePeriod.CurrentDay:
                     startTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
+                    endTime = startTime.Value.AddDays(1);
                     break;
                 case TimePeriod.PastMonth:
                     startTime = new DateTime(now.Year, now.Month, 1).AddMonths(-1);
-                    endTime = new DateTime(now.Year, now.Month, 1).AddSeconds(-1);
+                    endTime = new DateTime(now.Year, now.Month, 1);
                     break;
                 case TimePeriod.CurrentMonth:
                     startTime = new DateTime(now.Year, now.Month, 1);
+                    endTime = startTime.Value.AddMonths(1);
                     break;
                 case TimePeriod.PastQuarter:
                     int currentQuarter = (now.Month - 1) / 3 + 1;
                     startTime = new DateTime(now.Year, (currentQuarter - 2) * 3 + 1, 1);
-                    endTime = new DateTime(now.Year, (currentQuarter - 1) * 3 + 1, 1).AddSeconds(-1);
+                    endTime = new DateTime(now.Year, (currentQuarter - 1) * 3 + 1, 1);
                     break;
                 case TimePeriod.CurrentQuarter:
                     currentQuarter = (now.Month - 1) / 3 + 1;
                     startTime = new DateTime(now.Year, (currentQuarter - 1) * 3 + 1, 1);
+                    endTime = startTime.Value.AddMonths(3);
                     break;
                 case TimePeriod.Last3Days:
-                    startTime = now.AddDays(-3);
-                    endTime = null;
+                    startTime = DateTime.UtcNow.Date.AddDays(-3);
                     break;
                 case TimePeriod.Last7Days:
-                    startTime = now.AddDays(-7);
-                    endTime = null;
+                    startTime = DateTime.UtcNow.Date.AddDays(-7);
                     break;
                 case TimePeriod.Last14Days:
-                    startTime = now.AddDays(-14);
-                    endTime = null;
+                    startTime = DateTime.UtcNow.Date.AddDays(-14);
                     break;
-                case TimePeriod.TillLastMonday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Monday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
+                case TimePeriod.Last1Hours:
+                    lastPeriod = TimeSpan.FromHours(1);
                     break;
-                case TimePeriod.TillLastTuesday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Tuesday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
-                    break;
-                case TimePeriod.TillLastWednesday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Wednesday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
-                    break;
-                case TimePeriod.TillLastThursday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Thursday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
-                    break;
-                case TimePeriod.TillLastFriday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Friday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
-                    break;
-                case TimePeriod.TillLastSaturday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Saturday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
-                    break;
-                case TimePeriod.TillLastSunday:
-                    startTime = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Sunday);
-                    if (startTime > now) startTime = startTime.Value.AddDays(-7);
-                    startTime = new DateTime(startTime.Value.Year, startTime.Value.Month, startTime.Value.Day, 0, 0, 0);
+                case TimePeriod.Last2Hours:
+                    lastPeriod = TimeSpan.FromHours(2);
                     break;
                 case TimePeriod.Last3Hours:
-                    startTime = now.AddHours(-3);
-                    endTime = null;
+                    lastPeriod = TimeSpan.FromHours(3);
                     break;
                 case TimePeriod.Last6Hours:
-                    startTime = now.AddHours(-6);
-                    endTime = null;
+                    lastPeriod = TimeSpan.FromHours(6);
                     break;
                 case TimePeriod.Last12Hours:
-                    startTime = now.AddHours(-12);
-                    endTime = null;
+                    lastPeriod = TimeSpan.FromHours(12);
+                    break;
+                case TimePeriod.Last16Hours:
+                    lastPeriod = TimeSpan.FromHours(16);
+                    break;
+                case TimePeriod.Last24Hours:
+                    lastPeriod = TimeSpan.FromHours(24);
                     break;
                 case TimePeriod.Today:
                     startTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
-                    endTime = null;
+                    endTime = startTime.Value.AddDays(1);
                     break;
                 case TimePeriod.Yesterday:
                     startTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0).AddDays(-1);
-                    endTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0).AddSeconds(-1);
+                    endTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
                     break;
                 case TimePeriod.Everything:
                     startTime = null;
                     endTime = null;
                     break;
+                case TimePeriod.TillLastMonday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Monday, now);
+                    break;
+                case TimePeriod.TillLastTuesday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Tuesday, now);
+                    break;
+                case TimePeriod.TillLastWednesday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Wednesday, now);
+                    break;
+                case TimePeriod.TillLastThursday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Thursday, now);
+                    break;
+                case TimePeriod.TillLastFriday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Friday, now);
+                    break;
+                case TimePeriod.TillLastSaturday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Saturday, now);
+                    break;
+                case TimePeriod.TillLastSunday:
+                    startTime = GetLastDayOfWeek(DayOfWeek.Sunday, now);
+                    break;
             }
 
-            return (StartTime: startTime, EndTime: endTime);
+            return (startTime, endTime, lastPeriod);
+        }
+        private static DateTime GetLastDayOfWeek(DayOfWeek dayOfWeek, DateTime now) {
+            int daysUntilDayOfWeek = dayOfWeek - now.DayOfWeek;
+            if (daysUntilDayOfWeek > 0) daysUntilDayOfWeek -= 7;
+            return now.AddDays(daysUntilDayOfWeek);
         }
 
     }
+
 }
