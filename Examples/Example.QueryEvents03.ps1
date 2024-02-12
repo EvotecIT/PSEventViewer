b376633e8635dbb8c9caec9a3c2eef145bfe9d6b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-12 22:45:14 +0100

    Query for all logs

---
+++ Examples/Example.QueryEvents03.ps1
@@ -0,0 +1,4 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
+
+Find-WinEvent -LogName 'System' -Verbose -ParallelOption Parallel -MachineName 'EVOMONSTER', 'AD1' | Format-Table
