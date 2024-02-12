e3e4343a95e3ae490af4f0b82aab0dbb1cda02eb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-12 10:30:33 +0100

    Add examples for querying events in PowerShell

--- Examples/Example.QueryEvents01.ps1
+++ Examples/Example.QueryEvents01.ps1
@@ -1,6 +1,7 @@
 ﻿Clear-Host
 Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
 
+# those events will be split in 22 chunks to allow parallel processing and prevent errors
 $EventId = @(
     1..30
     5136, 5137, 5168
---
+++ Examples/Example.QueryEvents02.ps1
@@ -0,0 +1,6 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
+
+$Output = Find-WinEvent -LogName 'Security' -EventId 4627 -Verbose -MachineName 'AD1', 'AD2', 'AD0' -ParallelOption Parallel | Select-Object -First 2
+$Output[0] | Format-List
+$Output[0].MessageData
