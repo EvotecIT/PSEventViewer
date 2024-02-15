fbbe30b329170e8eb3fa804a63b2790d956fcfb7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-15 16:52:08 +0100

    Added listlog functionality

--- Sources/PSEventViewer.PowerShell/CmdletFindWinEvent.cs
+++ Sources/PSEventViewer.PowerShell/CmdletFindWinEvent.cs
@@ -3,9 +3,13 @@ using System;
 using System.Management.Automation;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Diagnostics.Eventing.Reader;
 
 namespace PSEventViewer.PowerShell {
 
+    //[OutputType(typeof(EventRecord), ParameterSetName = new string[] { "GetLogSet", "GetProviderSet", "FileSet", "HashQuerySet", "XmlQuerySet" })]
+    //[OutputType(typeof(ProviderMetadata), ParameterSetName = new string[] { "ListProviderSet" })]
+    //[OutputType(typeof(EventLogConfiguration), ParameterSetName = new string[] { "ListLogSet" })]
     [Cmdlet(VerbsCommon.Find, "WinEvent", DefaultParameterSetName = "GenericEvents")]
     public sealed class CmdletFindEvent : AsyncPSCmdlet {
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "RecordId")]
@@ -23,6 +27,7 @@ namespace PSEventViewer.PowerShell {
         [Parameter(Mandatory = false, ParameterSetName = "RecordId")]
         [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
         [Parameter(Mandatory = false, ParameterSetName = "NamedEvents")]
+        [Parameter(Mandatory = false, ParameterSetName = "ListLog")]
         public List<string> MachineName;
 
         [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
@@ -68,37 +73,48 @@ namespace PSEventViewer.PowerShell {
         [Parameter(Mandatory = true, ParameterSetName = "NamedEvents")]
         public NamedEvents[] Type;
 
+
+        [Parameter(Mandatory = true, ParameterSetName = "ListLog")]
+        public string[] ListLog { get; set; } = { "*" };
+
+
         protected override Task BeginProcessingAsync() {
             // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
             var internalLogger = new InternalLogger(false);
             var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
-            var eventSearching = new EventSearching(internalLogger);
+            var searchEvents = new SearchEvents(internalLogger);
             return Task.CompletedTask;
         }
         protected override Task ProcessRecordAsync() {
-            if (Type != null) {
-                // let's find the events prepared for search
-                List<NamedEvents> typeList = Type.ToList();
-                foreach (var eventObject in EventSearchingTargeted.FindEventsByNamedEvents(typeList, MachineName, StartTime, EndTime, TimePeriod, maxThreads: NumberOfThreads, maxEvents: MaxEvents)) {
-                    WriteObject(eventObject);
+            if (ParameterSetName == "ListLog") {
+                foreach (var log in SearchEvents.DisplayEventLogsParallel(ListLog, MachineName, NumberOfThreads)) {
+                    WriteObject(log);
                 }
             } else {
-                // Let's find the events by generic log name, event id, machine name, provider name, keywords, level, start time, end time, user id, and max events.
-                if (ParallelOption == ParallelOption.Disabled) {
-                    if (MachineName == null) {
-                        foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, null, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, EventRecordId, TimePeriod)) {
-                            WriteObject(eventObject);
-                        }
-                    } else {
-                        foreach (var machine in MachineName) {
-                            foreach (var eventObject in EventSearching.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, EventRecordId, TimePeriod)) {
+                if (Type != null) {
+                    // let's find the events prepared for search
+                    List<NamedEvents> typeList = Type.ToList();
+                    foreach (var eventObject in SearchEvents.FindEventsByNamedEvents(typeList, MachineName, StartTime, EndTime, TimePeriod, maxThreads: NumberOfThreads, maxEvents: MaxEvents)) {
+                        WriteObject(eventObject);
+                    }
+                } else {
+                    // Let's find the events by generic log name, event id, machine name, provider name, keywords, level, start time, end time, user id, and max events.
+                    if (ParallelOption == ParallelOption.Disabled) {
+                        if (MachineName == null) {
+                            foreach (var eventObject in SearchEvents.QueryLog(LogName, EventId, null, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, EventRecordId, TimePeriod)) {
                                 WriteObject(eventObject);
                             }
+                        } else {
+                            foreach (var machine in MachineName) {
+                                foreach (var eventObject in SearchEvents.QueryLog(LogName, EventId, machine, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, EventRecordId, TimePeriod)) {
+                                    WriteObject(eventObject);
+                                }
+                            }
                         }
-                    }
-                } else if (ParallelOption == ParallelOption.Parallel) {
-                    foreach (var eventObject in EventSearching.QueryLogsParallel(LogName, EventId, MachineName, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, NumberOfThreads, EventRecordId, TimePeriod)) {
-                        WriteObject(eventObject);
+                    } else if (ParallelOption == ParallelOption.Parallel) {
+                        foreach (var eventObject in SearchEvents.QueryLogsParallel(LogName, EventId, MachineName, ProviderName, Keywords, Level, StartTime, EndTime, UserId, MaxEvents, NumberOfThreads, EventRecordId, TimePeriod)) {
+                            WriteObject(eventObject);
+                        }
                     }
                 }
             }
