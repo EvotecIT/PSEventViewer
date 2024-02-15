b2380073f7eaf07fee385d2ae4bc7272d0c7781c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-15 16:51:42 +0100

    Test new search

---
+++ Sources/PSEventViewer.Examples/Examples.QueryEventSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSEventViewer.Examples {
+    internal partial class Examples {
+
+        public static void QueryBasicEventLogList() {
+
+            SearchEvents eventLogSettings = new SearchEvents();
+            eventLogSettings.Verbose = true;
+            eventLogSettings.Warning = true;
+            eventLogSettings.Error = true;
+
+            foreach (var test in SearchEvents.DisplayEventLogs()) {
+                Console.WriteLine(test);
+            }
+        }
+    }
+}
