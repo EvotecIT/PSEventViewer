46d47da4395e5ec57c10c4e17d46febc794b564d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-18 09:11:52 +0100

    Create placeholder

---
+++ Sources/PSEventViewer.PowerShell/CmdletWriteWinEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace PSEventViewer.PowerShell {
+    [Cmdlet(VerbsCommunications.Write, "WinEvent")]
+    public sealed class CmdletWriteWinEvent : AsyncPSCmdlet {
+        protected override Task BeginProcessingAsync() {
+            // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
+            var internalLogger = new InternalLogger(false);
+            var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+            var searchEvents = new SearchEvents(internalLogger);
+            return Task.CompletedTask;
+        }
+        protected override Task ProcessRecordAsync() {
+
+            return Task.CompletedTask;
+        }
+        protected override Task EndProcessingAsync() {
+
+            return Task.CompletedTask;
+        }
+    }
+}
