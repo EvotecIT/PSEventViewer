b7c505cdd928d40fe2c7b3a6fb5114c0cc1e6a7d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-05 16:27:12 +0100

    Add another example

---
+++ Examples/Example.QueryNamedEvents03.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
+
+$findWinEventSplat = @{
+    Type        = 'OSTimeChange'
+    MachineName = 'AD1', 'AD2', 'AD0'
+    Verbose     = $true
+}
+
+Find-WinEvent @findWinEventSplat -TimePeriod Last3Days  | Format-Table
