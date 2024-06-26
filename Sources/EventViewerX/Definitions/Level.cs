a2b005bbb8bb11f14d994645d284dcf9db519ee2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-26 22:28:34 +0200

    changed file format

--- Sources/EventViewerX/Definitions/Level.cs
+++ Sources/EventViewerX/Definitions/Level.cs
@@ -1,10 +1,10 @@
-﻿namespace EventViewerX {
-    public enum Level {
-        Verbose = 5,
-        Informational = 4,
-        Warning = 3,
-        Error = 2,
-        Critical = 1,
-        LogAlways = 0
-    }
+﻿namespace EventViewerX;
+
+public enum Level {
+    Verbose = 5,
+    Informational = 4,
+    Warning = 3,
+    Error = 2,
+    Critical = 1,
+    LogAlways = 0
 }
