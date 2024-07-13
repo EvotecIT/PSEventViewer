3b6f6535c8ed98468141f3cb35c05ba27011ff17
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-13 18:51:25 +0200

    move out of namespace

--- Sources/EventViewerX/Settings.cs
+++ Sources/EventViewerX/Settings.cs
@@ -1,38 +1,37 @@
-namespace EventViewerX {
+namespace EventViewerX;
 
-    public class Settings {
-        protected static InternalLogger _logger = new InternalLogger();
+public class Settings {
+    protected static InternalLogger _logger = new InternalLogger();
 
-        public bool Error {
-            get => _logger.IsError;
-            set => _logger.IsError = value;
-        }
-
-        public bool Verbose {
-            get => _logger.IsVerbose;
-            set => _logger.IsVerbose = value;
-        }
-
-        public bool Warning {
-            get => _logger.IsWarning;
-            set => _logger.IsWarning = value;
-        }
-
-        public bool Progress {
-            get => _logger.IsProgress;
-            set => _logger.IsProgress = value;
-        }
-
-        public bool Debug {
-            get => _logger.IsDebug;
-            set => _logger.IsDebug = value;
-        }
-
-        /// <summary>
-        /// Number of threads to use for lingering object detection
-        /// </summary>
-        public int NumberOfThreads = 8;
-
-        protected readonly object _LockObject = new object();
+    public bool Error {
+        get => _logger.IsError;
+        set => _logger.IsError = value;
     }
+
+    public bool Verbose {
+        get => _logger.IsVerbose;
+        set => _logger.IsVerbose = value;
+    }
+
+    public bool Warning {
+        get => _logger.IsWarning;
+        set => _logger.IsWarning = value;
+    }
+
+    public bool Progress {
+        get => _logger.IsProgress;
+        set => _logger.IsProgress = value;
+    }
+
+    public bool Debug {
+        get => _logger.IsDebug;
+        set => _logger.IsDebug = value;
+    }
+
+    /// <summary>
+    /// Number of threads to use for lingering object detection
+    /// </summary>
+    public int NumberOfThreads = 8;
+
+    protected readonly object _LockObject = new object();
 }
