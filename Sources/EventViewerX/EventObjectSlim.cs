1eb78df19a360aec8229ef202a3c0c9d5527fe60
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-19 11:52:02 +0200

    Rearrange namespace

--- Sources/EventViewerX/EventObjectSlim.cs
+++ Sources/EventViewerX/EventObjectSlim.cs
@@ -1,77 +1,76 @@
-﻿using System.Collections.Generic;
+﻿namespace EventViewerX;
 
-namespace EventViewerX {
-    public class EventObjectSlim {
-        internal EventObject _eventObject;
-        public int EventID; // = _eventObject.Id;
-        public long? RecordID; // = _eventObject.RecordId;
-        public string GatheredFrom; // = _eventObject.MachineName;
-        public string GatheredLogName; // = _eventObject.LogName;
-        public string Type;
+public class EventObjectSlim {
+    internal EventObject _eventObject;
+    public int EventID; // = _eventObject.Id;
+    public long? RecordID; // = _eventObject.RecordId;
+    public string GatheredFrom; // = _eventObject.MachineName;
+    public string GatheredLogName; // = _eventObject.LogName;
+    public string Type;
 
 
-        public EventObjectSlim(EventObject eventObject) {
-            _eventObject = eventObject;
-            EventID = _eventObject.Id;
-            RecordID = _eventObject.RecordId;
-            GatheredFrom = _eventObject.QueriedMachine;
-            GatheredLogName = _eventObject.ContainerLog;
+    public EventObjectSlim(EventObject eventObject) {
+        _eventObject = eventObject;
+        EventID = _eventObject.Id;
+        RecordID = _eventObject.RecordId;
+        GatheredFrom = _eventObject.QueriedMachine;
+        GatheredLogName = _eventObject.ContainerLog;
+    }
+
+    internal static string ConvertToObjectAffected(EventObject eventObject) {
+        if (eventObject.Data.ContainsKey("TargetUserName") && eventObject.Data.ContainsKey("TargetDomainName")) {
+            return eventObject.Data["TargetDomainName"] + "\\" + eventObject.Data["TargetUserName"];
+        } else if (eventObject.Data.ContainsKey("TargetUserName")) {
+            return eventObject.Data["TargetUserName"];
+        } else {
+            return "";
         }
-
-        internal static string ConvertToObjectAffected(EventObject eventObject) {
-            if (eventObject.Data.ContainsKey("TargetUserName") && eventObject.Data.ContainsKey("TargetDomainName")) {
-                return eventObject.Data["TargetDomainName"] + "\\" + eventObject.Data["TargetUserName"];
-            } else if (eventObject.Data.ContainsKey("TargetUserName")) {
-                return eventObject.Data["TargetUserName"];
-            } else {
-                return "";
-            }
+    }
+    internal static string ConvertToSamAccountName(EventObject eventObject) {
+        if (eventObject.Data.ContainsKey("SamAccountName")) {
+            return eventObject.Data["SamAccountName"];
+        } else {
+            return "";
         }
-        internal static string ConvertToSamAccountName(EventObject eventObject) {
-            if (eventObject.Data.ContainsKey("SamAccountName")) {
-                return eventObject.Data["SamAccountName"];
-            } else {
-                return "";
-            }
-        }
-        internal string ConvertFromOperationType(string s) {
-            Dictionary<string, string> known = new Dictionary<string, string> {
+    }
+    internal string ConvertFromOperationType(string s) {
+        Dictionary<string, string> known = new Dictionary<string, string> {
                 {"%%14674", "Value Added"},
                 {"%%14675", "Value Deleted"},
                 {"%%14676", "Unknown"}
             };
 
-            if (known.ContainsKey(s)) {
-                return known[s];
-            } else {
-                return "Unknown Operation";
-            }
+        if (known.ContainsKey(s)) {
+            return known[s];
+        } else {
+            return "Unknown Operation";
         }
-        internal static string OverwriteByField(string findField, string expectedValue, string currentValue, string insertValue) {
-            //OverwriteByField = [ordered] @{
-            //    'User Object' = 'Action', 'A directory service object was moved.', 'OldObjectDN'
-            //    'Field Value' = 'Action', 'A directory service object was moved.', 'NewObjectDN'
-            //}
-            if (findField == expectedValue) {
-                return insertValue;
-            } else {
-                return currentValue;
-            }
-
+    }
+    internal static string OverwriteByField(string findField, string expectedValue, string currentValue, string insertValue) {
+        //OverwriteByField = [ordered] @{
+        //    'User Object' = 'Action', 'A directory service object was moved.', 'OldObjectDN'
+        //    'Field Value' = 'Action', 'A directory service object was moved.', 'NewObjectDN'
+        //}
+        if (findField == expectedValue) {
+            return insertValue;
+        } else {
+            return currentValue;
         }
-        internal static string TranslateUacValue(string hexValue) {
-            if (hexValue == null || hexValue.Trim() == "-") {
-                return "";
-            }
-            // <Data Name="OldUacValue">0x10</Data> 
-            // <Data Name="NewUacValue">0x11</Data> 
-            // <Data Name="UserAccountControl">%%2080</Data> 
 
-            // Convert the hexadecimal value to an integer
-            int uacValue = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+    }
+    internal static string TranslateUacValue(string hexValue) {
+        if (hexValue == null || hexValue.Trim() == "-") {
+            return "";
+        }
+        // <Data Name="OldUacValue">0x10</Data> 
+        // <Data Name="NewUacValue">0x11</Data> 
+        // <Data Name="UserAccountControl">%%2080</Data> 
 
-            // Define the UAC flags
-            Dictionary<int, string> uacFlags = new Dictionary<int, string> {
+        // Convert the hexadecimal value to an integer
+        int uacValue = int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+
+        // Define the UAC flags
+        Dictionary<int, string> uacFlags = new Dictionary<int, string> {
                 { 0x0001, "SCRIPT" },
                 { 0x0002, "ACCOUNTDISABLE" },
                 { 0x0008, "HOMEDIR_REQUIRED" },
@@ -96,16 +95,15 @@ namespace EventViewerX {
                 { 0x04000000, "PARTIAL_SECRETS_ACCOUNT" }
             };
 
-            // Map each bit in the UAC value to a UAC flag
-            List<string> translatedFlags = new List<string>();
-            foreach (var flag in uacFlags) {
-                if ((uacValue & flag.Key) != 0) {
-                    translatedFlags.Add(flag.Value);
-                }
+        // Map each bit in the UAC value to a UAC flag
+        List<string> translatedFlags = new List<string>();
+        foreach (var flag in uacFlags) {
+            if ((uacValue & flag.Key) != 0) {
+                translatedFlags.Add(flag.Value);
             }
-
-            // Return the translated UAC flags as a comma-separated string
-            return string.Join(", ", translatedFlags);
         }
+
+        // Return the translated UAC flags as a comma-separated string
+        return string.Join(", ", translatedFlags);
     }
 }
