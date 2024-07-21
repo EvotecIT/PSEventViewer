746b401fba5c4ff2df5799edb4d5e93425469d70
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-21 17:21:39 +0200

    Convert Write-WinEvent to binary

--- Sources/PSEventViewer/CmdletWriteWinEvent.cs
+++ Sources/PSEventViewer/CmdletWriteWinEvent.cs
@@ -1,24 +1,46 @@
-﻿using System.Management.Automation;
-using System.Threading.Tasks;
-using EventViewerX;
+﻿namespace PSEventViewer;
 
-namespace PSEventViewer {
-    [Cmdlet(VerbsCommunications.Write, "WinEvent")]
-    public sealed class CmdletWriteWinEvent : AsyncPSCmdlet {
-        protected override Task BeginProcessingAsync() {
-            // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
-            var internalLogger = new InternalLogger(false);
-            var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
-            var searchEvents = new SearchEvents(internalLogger);
-            return Task.CompletedTask;
-        }
-        protected override Task ProcessRecordAsync() {
+[Alias("Write-Event")]
+[Cmdlet(VerbsCommunications.Write, "WinEvent")]
+public sealed class CmdletWriteWinEvent : AsyncPSCmdlet {
+    [Alias("ComputerName", "ServerName")]
+    [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
+    public string MachineName;
 
-            return Task.CompletedTask;
-        }
-        protected override Task EndProcessingAsync() {
+    [Parameter(Mandatory = true, Position = 0, ParameterSetName = "RecordId")]
+    [Parameter(Mandatory = true, Position = 0, ParameterSetName = "GenericEvents")]
+    public string LogName;
 
-            return Task.CompletedTask;
-        }
+    [Alias("Source", "Provider")]
+    [Parameter(Mandatory = true, ParameterSetName = "GenericEvents")]
+    public string ProviderName;
+
+    [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
+    public int Category;
+
+    [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
+    public System.Diagnostics.EventLogEntryType EventLogEntryType = System.Diagnostics.EventLogEntryType.Information;
+
+    [Alias("Id")]
+    [Parameter(Mandatory = true, ParameterSetName = "GenericEvents")]
+    public int EventId;
+
+    [Parameter(Mandatory = true, ParameterSetName = "GenericEvents")]
+    public string Message;
+
+    [Parameter(Mandatory = false, ParameterSetName = "GenericEvents")]
+    public string[] AdditionalFields;
+
+
+    protected override Task BeginProcessingAsync() {
+        // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
+        var internalLogger = new InternalLogger(false);
+        var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+        var searchEvents = new SearchEvents(internalLogger);
+        return Task.CompletedTask;
+    }
+    protected override Task ProcessRecordAsync() {
+        SearchEvents.WriteEvent(ProviderName, LogName, Message, EventLogEntryType, Category, EventId, MachineName, AdditionalFields);
+        return Task.CompletedTask;
     }
 }
