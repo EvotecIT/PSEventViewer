82872e668b38b156acfd4ff710d10bd32e04240a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-21 18:53:16 +0200

    Update example

--- Examples/Example.QueryNamedEvents01.ps1
+++ Examples/Example.QueryNamedEvents01.ps1
@@ -1,5 +1,15 @@
 ﻿Clear-Host
-#Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force -verbose
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force -Verbose
 
-#Find-WinEvent -Type ADComputerChangeDetailed, ADComputerCreateChange, ADUserLockouts, ADGroupCreateDelete , ADLdapBindingDetails -MachineName AD1, AD2, AD0 -Verbose |
+Find-WinEvent -Machine AD0 -Type ADUserLogonKerberos -TimePeriod Last1Hours -Verbose  # | Format-Table
 
+$Data = Find-WinEvent -MachineName AD0 -EventRecordId '28907707' -LogName Security
+
+#Find-WinEvent -Type ADComputerChangeDetailed, ADComputerCreateChange, ADUserLockouts, ADGroupCreateDelete , ADLdapBindingDetails -MachineName AD1, AD2, AD0 -Verbose | Format-Table
+
+# Measure-Command {
+#     Find-WinEvent -Type ADComputerChangeDetailed, ADComputerCreateChange, ADUserLockouts, ADGroupCreateDelete -MachineName AD1, AD2, AD0 -Verbose -StartTime (Get-Date).AddDays(-1)
+
+# }
+
+Measure-Command { $T = Find-WinEvent -Machine AD0 -LogName Security -EventId 5136, 5137, 5139, 5141, 4741, 4742, 4740, 4727, 4730, 4731, 4734, 4744, 4748, 4749, 4753, 4754, 4758, 4759, 4763 -DateFrom (Get-Date).AddDays(-1) -DateTo (Get-Date) -Verbose }
