e399b5d543f9fe154423453960c1dcb1744a21e1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-25 13:00:27 +0100

    Add example script for querying SMB Server Audit events

---
+++ Examples/Example.QueryNamedEventsSMB01.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
+
+$findWinEventSplat = @{
+    Type        = 'ADSMBServerAuditV1'
+    MachineName = 'AD1', 'AD2', 'AD0'
+    Verbose     = $true
+}
+
+Find-WinEvent @findWinEventSplat -TimePeriod Last3Days
