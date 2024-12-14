d8566e62248c61a0ff8c9386c56fd01d16a7b435
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-14 12:52:27 +0100

    Add logging classes for cleared and full security logs
    This commit introduces namespace declarations and class definitions for logging events related to cleared security logs, cleared application/system logs, and full security logs. Each class includes properties for `Computer`, `Action`, `BackupPath`, `LogType`, `Who`, and `When`, with constructors that initialize these properties from the provided `EventObject`. Special cases are handled, such as setting `Who` to "Automatic Backup" for event ID 1105 and defaulting `BackupPath` to "N/A" if empty. XML documentation comments have also been added for clarity.

---
+++ Sources/EventViewerX/Rules/Logging/LogsFullSecurity.cs
@@ -0,0 +1,26 @@
+﻿namespace EventViewerX.Rules.Logging;
+
+/// <summary>
+/// Logs Security Full
+/// 1104: The security log is now full
+/// </summary>
+public class LogsFullSecurity : EventObjectSlim {
+    public string Computer;
+    public string Action;
+    public string LogType;
+    public string Who;
+    public DateTime When;
+
+    public LogsFullSecurity(EventObject eventObject) : base(eventObject) {
+        _eventObject = eventObject;
+
+        Type = "LogsFullSecurity";
+        Computer = _eventObject.ComputerName;
+        Action = _eventObject.MessageSubject;
+        LogType = ConvertFromOperationType(_eventObject.Data["Channel"]);
+
+        // common fields
+        Who = _eventObject.GetValueFromDataDictionary("SubjectUserName", "SubjectDomainName", "\\", reverseOrder: true);
+        When = _eventObject.TimeCreated;
+    }
+}
