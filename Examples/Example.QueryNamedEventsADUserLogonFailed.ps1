d4408fe0ec032c39ad862a93364cd48bcd875fff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-14 09:56:55 +0100

    Add example script for querying ADUserLogonFailed events

---
+++ Examples/Example.QueryNamedEventsADUserLogonFailed.ps1
@@ -0,0 +1,11 @@
+﻿
+Clear-Host
+Import-Module $PSScriptRoot\..\PSEventViewer.psd1 -Force
+
+$findWinEventSplat = @{
+    Type        = 'ADUserLogonFailed'
+    MachineName = 'DC1', 'DC2'
+    Verbose     = $true
+}
+
+Find-WinEvent @findWinEventSplat -TimePeriod Last7Days | Format-Table *
