16a4bc08792dd7ecb74d1743dfe13301d4356d09
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-14 17:56:56 +0100

    Refactor event logging classes in EventViewerX
    Restructured and reformatted classes in the `EventViewerX.Rules.Windows` namespace. Added namespace declarations and improved documentation comments for `ClientGroupPolicies`, `OSStartupShutdownCrash`, `OSCrash`, and `OSTimeChange`. Updated constructors to initialize properties from `EventObject`. Removed commented-out code to enhance readability and maintainability.

---
+++ Sources/EventViewerX/Rules/Windows/OSCrash.cs
@@ -0,0 +1,30 @@
+﻿namespace EventViewerX.Rules.Windows;
+
+/// <summary>
+/// Windows OS Crash
+/// 6008: The previous system shutdown at time on date was unexpected.
+/// </summary>
+public class OSCrash : EventObjectSlim {
+    public string Computer;
+    public string Action;
+    public string ObjectAffected;
+    public string ActionDetails;
+    public string ActionDetailsDate;
+    public string ActionDetailsTime;
+    public string Who;
+    public DateTime When;
+
+    public OSCrash(EventObject eventObject) : base(eventObject) {
+        _eventObject = eventObject;
+
+        Type = "OSCrash";
+        Computer = _eventObject.ComputerName;
+        Action = _eventObject.GetValueFromDataDictionary("EventAction");
+        ObjectAffected = _eventObject.MachineName;
+        ActionDetails = _eventObject.MessageSubject;
+        ActionDetailsDate = _eventObject.GetValueFromDataDictionary("NoNameA1");
+        ActionDetailsTime = _eventObject.GetValueFromDataDictionary("NoNameA0");
+
+        When = _eventObject.TimeCreated;
+    }
+}
