7c75f8f605f33c1b8f4839044656b5f67501ae18
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-24 12:41:57 +0100

    Add GpoLink class to represent group policy links with relevant properties

---
+++ Sources/EventViewerX/Definitions/GpoLink.cs
@@ -0,0 +1,8 @@
+﻿namespace EventViewerX;
+
+public class GpoLink {
+    public string DisplayName { get; set; } = string.Empty;
+    public string Guid { get; set; } = string.Empty;
+    public string DistinguishedName { get; set; } = string.Empty;
+    public bool IsEnabled { get; set; }
+}
