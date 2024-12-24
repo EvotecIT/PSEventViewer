09e860c4289440caffab99082d64464aaf91343a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-24 12:59:06 +0100

    Add GroupPolicy class to represent GPO details including name, ID, and domain

---
+++ Sources/EventViewerX/Definitions/GroupPolicy.cs
@@ -0,0 +1,10 @@
+﻿namespace EventViewerX;
+
+/// <summary>
+/// Represents a single Group Policy with its name, ID, and domain.
+/// </summary>
+public class GroupPolicy {
+    public string GpoName { get; set; }
+    public string GpoId { get; set; }
+    public string GpoDomain { get; set; }
+}
