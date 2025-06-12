e9d14d2e63a5502cc7436ded81db1349955f188d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-12 08:43:44 +0200

    Add new Get-WinEventFilter tests

--- Sources/EventViewerX.Tests/TestWinEventFilter.cs
+++ Sources/EventViewerX.Tests/TestWinEventFilter.cs
@@ -30,5 +30,26 @@ namespace EventViewerX.Tests {
             var result = SearchEvents.BuildWinEventFilter(namedDataExcludeFilter: [ht], logName: "xx", xpathOnly: true);
             Assert.Equal("*[EventData[Data[@Name='FieldName'] != 'Value1' and Data[@Name='FieldName'] != 'Value2']]", result);
         }
-    }
+        [Fact]
+        public void PathQueryUsesFilePrefix() {
+            var ht = new Hashtable { { "param4", "BITS" } };
+            var result = SearchEvents.BuildWinEventFilter(id: ["7040"], namedDataExcludeFilter: [ht], path: "C:/file.evtx");
+            Assert.Contains("Path=\"file://C:/file.evtx\"", result);
+            Assert.DoesNotContain("Select Path", result);
+        }
+
+        [Fact]
+        public void LogNameQueryAddsSelectPath() {
+            var ht = new Hashtable { { "param4", "BITS" } };
+            var result = SearchEvents.BuildWinEventFilter(id: ["7040"], namedDataExcludeFilter: [ht], logName: "System");
+            Assert.Contains("Path=\"System\"", result);
+            Assert.Contains("Select Path=\"System\"", result);
+        }
+
+        [Fact]
+        public void IdMultipleValuesOr() {
+            var result = SearchEvents.BuildWinEventFilter(id: ["1", "2"], logName: "x", xpathOnly: true);
+            Assert.Equal("*[System[(EventID=1) or (EventID=2)]]", result);
+        }
+}
 }
--- Tests/Get-WinEventFilter.Tests.ps1
+++ Tests/Get-WinEventFilter.Tests.ps1
@@ -1,32 +1,42 @@
-﻿Describe 'Get-EventsFilters' {
+﻿Describe 'Get-WinEventFilters' {
     It '-NamedDataFilter single value should return single named filter query "="' {
-        $XPath = Get-EventsFilter -NamedDataFilter @{ FieldName = 'Value1' } -LogName 'xx' -XPathOnly
+        $XPath = Get-WinEventFilter -NamedDataFilter @{ FieldName = 'Value1' } -LogName 'xx' -XPathOnly
         $Xpath | Should -Be '*[EventData[Data[@Name=''FieldName''] = ''Value1'']]'
     }
     It '-NamedDataFilter two-value array should return "or"ed named filter query "="' {
-        $XPath = Get-EventsFilter -NamedDataFilter @{ FieldName = ('Value1','Value2') } -LogName 'xx' -XPathOnly
+        $XPath = Get-WinEventFilter -NamedDataFilter @{ FieldName = ('Value1','Value2') } -LogName 'xx' -XPathOnly
         $Xpath | Should -Be '*[EventData[Data[@Name=''FieldName''] = ''Value1'' or Data[@Name=''FieldName''] = ''Value2'']]'
     }
     It '-NamedDataExcludeFilter should return single named filter query "!="' {
-        $XPath = Get-EventsFilter -NamedDataExcludeFilter @{ FieldName = 'Value1' } -LogName 'xx' -XPathOnly
+        $XPath = Get-WinEventFilter -NamedDataExcludeFilter @{ FieldName = 'Value1' } -LogName 'xx' -XPathOnly
         $Xpath | Should -Be '*[EventData[Data[@Name=''FieldName''] != ''Value1'']]'
     }
     It '-NamedDataExcludeFilter two-value array should return "and"ed named filter query "!="' {
-        $XPath = Get-EventsFilter -NamedDataExcludeFilter @{ FieldName = ('Value1','Value2') } -LogName 'xx' -XPathOnly
+        $XPath = Get-WinEventFilter -NamedDataExcludeFilter @{ FieldName = ('Value1','Value2') } -LogName 'xx' -XPathOnly
         $Xpath | Should -Be '*[EventData[Data[@Name=''FieldName''] != ''Value1'' and Data[@Name=''FieldName''] != ''Value2'']]'
     }
 }
 
 Describe "Get-EventFilters using Path and NamendDataFilter" {
     It "basic syntax check for queries of eventlogs" {
-        $XML = Get-EventsFilter -logname System -Id 7040 -NamedDataExcludeFilter @{ param4 = 'BITS' }
+        $XML = Get-WinEventFilter -logname System -Id 7040 -NamedDataExcludeFilter @{ param4 = 'BITS' }
         $XML | Should -BeLike '*Query Id="0" Path="system"*'    -Because 'We wanted to query a filepath'
         $XML | Should -BeLike '*Select Path="system"*'          -Because 'Queries using eventfiles do not have a Channel'
     }
     It "basic syntax check for queries of saved eventlog files" {
         $FilePath = [System.IO.Path]::Combine($PSScriptRoot, 'Logs', 'NamedFilterExamples.evtx')
-        $XML = Get-EventsFilter -Path $FilePath -Id 7040 -NamedDataExcludeFilter @{ param4 = 'BITS' }
+        $XML = Get-WinEventFilter -Path $FilePath -Id 7040 -NamedDataExcludeFilter @{ param4 = 'BITS' }
         $XML | Should -BeLike '*Query Id="0" Path="file://*'    -Because 'We wanted to query a filepath'
         $XML | Should -Not -Belike '*Select Path*'              -Because 'Queries using eventfiles do not have a Channel'
+    }
+}
+Describe 'Additional Get-WinEventFilter cases' {
+    It '-ID multiple values produce "or" xpath filter' {
+        $XPath = Get-WinEventFilter -ID 1,2 -LogName 'xx' -XPathOnly
+        $XPath | Should -Be '*[System[(EventID=1) or (EventID=2)]]'
+    }
+    It '-ExcludeID uses inequality syntax' {
+        $XPath = Get-WinEventFilter -ExcludeID 1,2 -LogName 'xx' -XPathOnly
+        $XPath | Should -Be '*[System[(EventID!=1) or (EventID!=2)]]'
     }
 }
