10ec481a6947e436510da7fa41154a1d19246918
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-27 20:31:18 +0200

    refactor(EventViewerX): 🔧 Update test cases to use Get-EVXEvent cmdlet
    * Renamed test cases from `Get-Events` to `Get-EVXEvent` for consistency with the updated cmdlet naming.
    * Adjusted parameters in tests to align with the new cmdlet functionality, including the addition of `-AsArray` in relevant tests.
    * Enhanced clarity and maintainability of the test suite by reflecting the latest cmdlet changes.

--- Tests/Get-Events.Tests.ps1
+++ Tests/Get-Events.Tests.ps1
@@ -1,8 +1,8 @@
-﻿Describe 'Get-Events - Basic Test' {
+﻿Describe 'Get-EVXEvent - Basic Test' {
     $Date = (Get-Date).AddDays(-60)
     $Date1 = Get-Date
 
-    $Events = Get-Events -Machine $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' # -Verbose
+    $Events = Get-EVXEvent -Machine $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' # -Verbose
 
     $PSDefaultParameterValues = @{
         "It:TestCases" = @{ Date = $Date; Date1 = $Date1; Events = $Events }
@@ -28,11 +28,11 @@
         $Events[0].ID | Should -Be 5617
     }
 }
-Describe 'Get-Events - MaxEvents Test' {
+Describe 'Get-EVXEvent - MaxEvents Test' {
     $Date = (Get-Date).AddDays(-60)
     $Date1 = Get-Date
 
-    $Events = Get-Events -Machine $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' -MaxEvents 1 #-Verbose
+    $Events = Get-EVXEvent -Machine $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' -MaxEvents 1 -AsArray
 
     $PSDefaultParameterValues = @{
         "It:TestCases" = @{ Date = $Date; Date1 = $Date1; Events = $Events }
@@ -63,7 +63,7 @@ Describe 'Get-Events - MaxEvents on 3 servers' {
     $Date = (Get-Date).AddDays(-60)
     $Date1 = Get-Date
 
-    $Events = Get-Events -Machine $Env:COMPUTERNAME, $Env:COMPUTERNAME, $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' -MaxEvents 1 #-Verbose
+    $Events = Get-EVXEvent -Machine $Env:COMPUTERNAME, $Env:COMPUTERNAME, $Env:COMPUTERNAME -DateFrom $Date -DateTo $Date1 -ID 5617 -LogName 'Application' -MaxEvents 1 -AsArray
 
     $PSDefaultParameterValues = @{
         "It:TestCases" = @{ Date = $Date; Date1 = $Date1; Events = $Events }
@@ -100,7 +100,7 @@ Describe 'Get-Events - MaxEvents on 3 servers' {
     }
 }
 
-Describe 'Get-Events - Read events from path (oldest / newest)' {
+Describe 'Get-EVXEvent - Read events from path (oldest / newest)' {
     $FilePath = [System.IO.Path]::Combine($PSScriptRoot, 'Logs', 'Active Directory Web Services.evtx')
 
     $PSDefaultParameterValues = @{
@@ -123,45 +123,45 @@ Describe 'Get-Events - Read events from path (oldest / newest)' {
         $EventsNewest[0].GatheredFrom | Should -Be $FilePath
 
         $EventsNewest[0].NoNameA0 | Should -Be 'GC'
-        $EventsNewest[0].NoNameA1 | Should -Be  3268
-        $EventsNewest[0].NoNameA2 | Should -Be  3269
+        $EventsNewest[0].NoNameA1 | Should -Be 3268
+        $EventsNewest[0].NoNameA2 | Should -Be 3269
     }
 }
 
-Describe 'Get-Events - Read events with NamedDataFilter' {
+Describe 'Get-EVXEvent - Read events with NamedDataFilter' {
     $FilePath = [System.IO.Path]::Combine($PSScriptRoot, 'Logs', 'NamedFilterExamples.evtx')
 
     $PSDefaultParameterValues = @{
         "It:TestCases" = @{ FilePath = $FilePath; }
     }
 
-   It 'No path and no logname should fail' {
-        Get-Events -Path $null -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err -ErrorAction SilentlyContinue
+    It 'No path and no logname should fail' {
+        Get-EVXEvent -Path $null -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err -ErrorAction SilentlyContinue
         $err | Should -Not -BeNullOrEmpty
     }
 
     It 'Using -Path should not fail' {
-        Get-Events -Path $FilePath -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err
+        Get-EVXEvent -Path $FilePath -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err
         $err | Should -BeNullOrEmpty
     }
 
     It 'Using named filter and -Path should return something like "No events were found"' {
-        Get-Events -Path $FilePath -NamedDataExcludeFilter @{ Data0 = ('blabla','blublu') } -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err
+        Get-EVXEvent -Path $FilePath -NamedDataExcludeFilter @{ Data0 = ('blabla', 'blublu') } -Oldest -MaxEvents 1 -DisableParallel -ErrorVariable err
         $err | Should -BeLike '*No events were found*'
     }
 
     It 'named exclude filter' {
-        $ret = Get-Events -Path $FilePath -Id 7040 -NamedDataExcludeFilter @{ param4 = ('BITS','TrustedInstaller') } -MaxEvents 1
-        $ret                            | Should -HaveCount 1
+        $ret = Get-EVXEvent -Path $FilePath -Id 7040 -NamedDataExcludeFilter @{ param4 = ('BITS', 'TrustedInstaller') } -MaxEvents 1
+        $ret | Should -HaveCount 1
         ( [datetime] $ret.TimeCreated ) | Should -Be ( [datetime] "2019-08-30T06:57:44.037957100Z" )
-        $ret.param4                     | Should -Be 'NgcCtnrSvc'
+        $ret.param4 | Should -Be 'NgcCtnrSvc'
 
     }
     It 'named include filter' {
-        $ret = Get-Events -Path $FilePath -Id 7040 -NamedDataFilter @{ param4 = ('BITS','TrustedInstaller') } -oldest -MaxEvents 1
-        $ret                            | Should -HaveCount 1
+        $ret = Get-EVXEvent -Path $FilePath -Id 7040 -NamedDataFilter @{ param4 = ('BITS', 'TrustedInstaller') } -oldest -MaxEvents 1
+        $ret | Should -HaveCount 1
         ( [datetime] $ret.TimeCreated ) | Should -Be ( [datetime] "2019-08-30T06:50:13.213617700Z" )
-        $ret.param4                     | Should -Be 'BITS'
+        $ret.param4 | Should -Be 'BITS'
 
     }
 }
