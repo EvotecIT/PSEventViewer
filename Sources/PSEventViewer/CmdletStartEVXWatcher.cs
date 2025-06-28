efdc6a728b9299dd131bf24c2608d1445472da10
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-28 09:25:45 +0200

    feat(EventViewerX): ✨ Add CmdletStartEVXWatcher for real-time event log monitoring
    * Introduced CmdletStartEVXWatcher to enable real-time monitoring of Windows Event Logs with customizable filters and actions.
    * Supports concurrent event watching with a configurable number of threads for improved performance.
    * Provides a framework for executing scriptblocks in response to matched events, enhancing automation capabilities.

---
+++ Sources/PSEventViewer/CmdletStartEVXWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Management.Automation;
+using System.Threading.Tasks;
+using EventViewerX;
+
+namespace PSEventViewer {
+    /// <summary>
+    /// Starts real-time monitoring of Windows Event Logs with customizable filters and actions.
+    /// Provides continuous event watching with scriptblock execution for matched events.
+    /// </summary>
+    [Cmdlet(VerbsLifecycle.Start, "EVXWatcher")]
+    [Alias("Start-EventViewerXWatcher", "Start-EventWatching")]
+    [OutputType(typeof(void))]
+    public sealed class CmdletStartEVXWatcher : AsyncPSCmdlet {
+        private WatchEvents EventWatching { get; set; }
+
+        [Parameter(Mandatory = false, Position = 1)] public int NumberOfThreads { get; set; } = 8;
+
+        protected override Task BeginProcessingAsync() {
+            // Initialize the logger to be able to see verbose, warning, debug, error, progress, and information messages.
+            var internalLogger = new InternalLogger(false);
+            var internalLoggerPowerShell = new InternalLoggerPowerShell(internalLogger, this.WriteVerbose, this.WriteWarning, this.WriteDebug, this.WriteError, this.WriteProgress, this.WriteInformation);
+
+            EventWatching = new WatchEvents(internalLogger) {
+                NumberOfThreads = NumberOfThreads
+            };
+            return Task.CompletedTask;
+        }
+        protected override Task ProcessRecordAsync() {
+
+            return Task.CompletedTask;
+        }
+        protected override Task EndProcessingAsync() {
+
+            return Task.CompletedTask;
+        }
+    }
+}
