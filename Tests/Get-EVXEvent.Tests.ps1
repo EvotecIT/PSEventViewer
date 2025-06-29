4ea1b62ce11579917f7063ba2a345d1c1cb33fab
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-29 11:54:39 +0200

    Fix message regex test setup

--- Tests/Get-EVXEvent.Tests.ps1
+++ Tests/Get-EVXEvent.Tests.ps1
@@ -157,10 +157,9 @@ Describe 'Get-EVXEvent - Read events with NamedDataFilter' {
 }
 
 Describe 'Get-EVXEvent - MessageRegex' {
-    $FilePath = [System.IO.Path]::Combine($PSScriptRoot, 'Logs', 'Active Directory Web Services.evtx')
-
     It 'Supports filtering by message regex' {
-        $events = Get-EVXEvent -Path $FilePath -MaxEvents 1 -MessageRegex '.*'
+        $FilePath = [System.IO.Path]::Combine($PSScriptRoot, 'Logs', 'Active Directory Web Services.evtx')
+        $events   = Get-EVXEvent -Path $FilePath -MaxEvents 1 -MessageRegex '.*'
         $events.Count | Should -Be 1
     }
 }
