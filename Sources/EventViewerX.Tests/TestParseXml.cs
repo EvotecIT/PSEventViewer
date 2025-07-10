1517af5b6789ed0375f3f86f2cd90a867422e9ac
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-10 20:03:53 +0200

    fix(core): 🐛 ensure XML data parsing ignores key case

---
+++ Sources/EventViewerX.Tests/TestParseXml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace EventViewerX.Tests {
+    public class TestParseXml {
+        [Fact]
+        public void DataDictionaryIsCaseInsensitive() {
+            const string xml = "<Event><EventData><Data Name='Key'>Value</Data></EventData></Event>";
+            var obj = (EventObject)FormatterServices.GetUninitializedObject(typeof(EventObject));
+            var method = typeof(EventObject).GetMethod("ParseXML", BindingFlags.NonPublic | BindingFlags.Instance)!
+                .MakeGenericMethod(typeof(Dictionary<string, string>));
+            var result = (Dictionary<string, string>)method.Invoke(obj, new object[] { xml })!;
+            Assert.Equal("Value", result["Key"]);
+            Assert.Equal("Value", result["key"]);
+        }
+    }
+}
