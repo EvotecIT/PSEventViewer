09497f4da221439e4f59005ee8247206e543d34b
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-10 22:05:00 +0200

    refactor(search): 🔨 reuse parsed XML

--- Sources/EventViewerX.Tests/TestPowerShellScripts.cs
+++ Sources/EventViewerX.Tests/TestPowerShellScripts.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using Xunit;
 
 namespace EventViewerX.Tests {
@@ -35,6 +36,37 @@ namespace EventViewerX.Tests {
             } finally {
                 Settings._logger.OnWarningMessage -= handler;
             }
+        }
+
+        [Fact]
+        public void ExtractDataFromElement() {
+            const string xml = "<Event><EventData><Data Name='Field'>Value</Data></EventData></Event>";
+            var element = XElement.Parse(xml);
+            var method = typeof(SearchEvents).GetMethod(
+                "ExtractData",
+                BindingFlags.NonPublic | BindingFlags.Static,
+                null,
+                new[] { typeof(XElement), typeof(string) },
+                null);
+            Assert.NotNull(method);
+            var result = method!.Invoke(null, new object[] { element, "Field" });
+            Assert.Equal("Value", result);
+        }
+
+        [Fact]
+        public void GetAllDataFromElement() {
+            const string xml = "<Event><EventData><Data Name='A'>1</Data><Data Name='B'>2</Data></EventData></Event>";
+            var element = XElement.Parse(xml);
+            var method = typeof(SearchEvents).GetMethod(
+                "GetAllData",
+                BindingFlags.NonPublic | BindingFlags.Static,
+                null,
+                new[] { typeof(XElement) },
+                null);
+            Assert.NotNull(method);
+            var result = (Dictionary<string, string?>)method!.Invoke(null, new object[] { element })!;
+            Assert.Equal("1", result["A"]);
+            Assert.Equal("2", result["B"]);
         }
     }
 }
