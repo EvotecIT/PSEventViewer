9e418bae4326ccd1854cd4776a7a14d943b20b53
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-16 12:07:23 +0200

    refactor(definitions): 🔨 split event enums into dedicated files

---
+++ Sources/EventViewerX/Definitions/BitLockerVolumeType.cs
@@ -0,0 +1,10 @@
+namespace EventViewerX;
+
+/// <summary>
+/// Types of volumes protected by BitLocker.
+/// </summary>
+public enum BitLockerVolumeType {
+    OperatingSystemVolume = 810,
+    FixedDataVolume = 811,
+    RemovableDataVolume = 812
+}
