93d65ffe6548a2b0d202ec50dfb68837fca3e459
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-02-13 10:09:56 +0100

    chore(version): 🔧 update project version to 3.3.0
    * Updated the project version in `UpdateVersion.ps1` to reflect the new release.
    * Ensured compatibility with existing modules and scripts.

--- Sources/Build/UpdateVersion.ps1
+++ Sources/Build/UpdateVersion.ps1
@@ -3,4 +3,4 @@
 $Path = "$PSScriptRoot\..\EventViewerX"
 
 Get-ProjectVersion -Path "$Path" -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
-Set-ProjectVersion -Path "$Path" -NewVersion "3.2.0" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
+Set-ProjectVersion -Path "$Path" -NewVersion "3.3.0" -WhatIf:$false -Verbose -ExcludeFolders @("$Path\Module\Artefacts") | Format-Table
