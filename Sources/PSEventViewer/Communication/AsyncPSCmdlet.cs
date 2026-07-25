d0f40e7f5a31e024450dfef865fcde4d250e0cb0
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-07-26 01:36:18 +0200

    Preserve async hook affinity and write ordering

--- Sources/PSEventViewer/Communication/AsyncPSCmdlet.cs
+++ Sources/PSEventViewer/Communication/AsyncPSCmdlet.cs
@@ -30,6 +30,17 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
         public override void Post(SendOrPostCallback callback, object? state)
             => ThreadPool.QueueUserWorkItem(_ => callback(state));
     }
+    private sealed class AsyncHookTaskScheduler : TaskScheduler
+    {
+        protected override System.Collections.Generic.IEnumerable<Task>? GetScheduledTasks()
+            => null;
+
+        protected override void QueueTask(Task task)
+            => ThreadPool.QueueUserWorkItem(_ => TryExecuteTask(task));
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+            => TryExecuteTask(task);
+    }
 
     private enum PipelineType
     {
@@ -126,6 +137,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     private InternalLogger? _eventViewerLogger;
     private Guid _powerShellResourceOwnerId;
     private static readonly SynchronizationContext HookSynchronizationContext = new AsyncHookSynchronizationContext();
+    private static readonly TaskScheduler HookTaskScheduler = new AsyncHookTaskScheduler();
     private BlockingCollection<PipelineItem>? _currentOutPipe;
     private bool _cancelSourceDisposed;
     private bool _disposeRequested;
@@ -299,7 +311,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe output bridge for asynchronous cmdlet code.</summary>
     public new void WriteObject(object? sendToPipeline, bool enumerateCollection)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteObject(sendToPipeline, enumerateCollection);
@@ -317,7 +329,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe error bridge for asynchronous cmdlet code.</summary>
     public new void WriteError(ErrorRecord errorRecord)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteError(errorRecord);
@@ -353,7 +365,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe warning bridge for asynchronous cmdlet code.</summary>
     public new void WriteWarning(string text)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteWarning(text);
@@ -369,7 +381,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe verbose bridge for asynchronous cmdlet code.</summary>
     public new void WriteVerbose(string text)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteVerbose(text);
@@ -385,7 +397,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe debug bridge for asynchronous cmdlet code.</summary>
     public new void WriteDebug(string text)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteDebug(text);
@@ -401,7 +413,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe information bridge for asynchronous cmdlet code.</summary>
     public new void WriteInformation(InformationRecord informationRecord)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteInformation(informationRecord);
@@ -417,7 +429,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe information bridge for asynchronous cmdlet code.</summary>
     public new void WriteInformation(object messageData, string[] tags)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteInformation(messageData, tags);
@@ -433,7 +445,7 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
     /// <summary>Thread-safe progress bridge for asynchronous cmdlet code.</summary>
     public new void WriteProgress(ProgressRecord progressRecord)
     {
-        if (CanAccessPipelineDirectly)
+        if (CanAccessPipelineDirectly && Volatile.Read(ref _currentOutPipe) is null)
         {
             ThrowIfStopped();
             base.WriteProgress(progressRecord);
@@ -708,14 +720,19 @@ public abstract class AsyncPSCmdlet : PSCmdlet, IDisposable
             SynchronizationContext.SetSynchronizationContext(HookSynchronizationContext);
             if (_eventViewerLogger is not null)
                 Settings.Logger = _eventViewerLogger;
-            blockTask = TaskScheduler.Current == TaskScheduler.Default
-                ? task()
-                : Task.Factory.StartNew(
-                        task,
-                        CancellationToken.None,
-                        TaskCreationOptions.DenyChildAttach,
-                        TaskScheduler.Default)
-                    .Unwrap();
+            if (TaskScheduler.Current == TaskScheduler.Default)
+            {
+                blockTask = task();
+            }
+            else
+            {
+                var invocationTask = new Task<Task>(
+                    task,
+                    CancellationToken.None,
+                    TaskCreationOptions.DenyChildAttach);
+                invocationTask.RunSynchronously(HookTaskScheduler);
+                blockTask = invocationTask.GetAwaiter().GetResult();
+            }
         }
         catch (Exception exception)
         {
