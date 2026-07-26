97957ab58b82a4d14d0feea9a58941e0d7503fce
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2026-07-26 07:13:21 +0200

    Sync final async lifecycle race fixes

--- Sources/PSEventViewer/Communication/AsyncPSCmdlet.Pipeline.cs
+++ Sources/PSEventViewer/Communication/AsyncPSCmdlet.Pipeline.cs
@@ -510,7 +510,8 @@ public abstract partial class AsyncPSCmdlet
 
         void PumpQueuedItems()
         {
-            while (outPipe.TryTake(out var item))
+            var queuedAtEntry = outPipe.Count;
+            while (queuedAtEntry-- > 0 && outPipe.TryTake(out var item))
                 PumpItem(item);
         }
 
@@ -548,6 +549,11 @@ public abstract partial class AsyncPSCmdlet
         }
         catch (Exception exception)
         {
+            lock (_hookAdmissionLock)
+            {
+                _ = Interlocked.CompareExchange(ref _acceptingHookWritesGeneration, 0, hookGeneration);
+            }
+            SynchronizationContext.SetSynchronizationContext(synchronizationContext);
             try
             {
                 PumpQueuedItems();
@@ -609,10 +615,14 @@ public abstract partial class AsyncPSCmdlet
             _ = blockTask.ContinueWith(
                 completed =>
                 {
+                    var retainedBlockOwned = true;
                     try
                     {
                         if (completed.IsFaulted)
                             _ = completed.Exception;
+
+                        ExitAsyncBlock();
+                        retainedBlockOwned = false;
 
                         lock (_hookAdmissionLock)
                         {
@@ -647,7 +657,8 @@ public abstract partial class AsyncPSCmdlet
                     }
                     finally
                     {
-                        ExitAsyncBlock();
+                        if (retainedBlockOwned)
+                            ExitAsyncBlock();
                     }
                 },
                 CancellationToken.None,
